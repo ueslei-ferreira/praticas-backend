9bf2d66441bf62d2085f4e58a3fc539201493bc1
Ueslei GIT <ueslei392@gmail.com>
2025-04-16 15:28:06 -0300

    add: esqueleto django; add: tabelas sql

---
+++ tarefas/tabelas.sql
@@ -0,0 +1,22 @@
+
+CREATE TABLE usuario(
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(50) UNIQUE NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    senha_hash TEXT NOT NULL,
+    data_criacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+);
+
+
+CREATE TABLE tarefa(
+    id SERIAL PRIMARY KEY,
+    titulo VARCHAR(200) NOT NULL,
+    descricao TEXT NOT NULL,
+    data_criacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    prazo TIMESTAMP,
+    concluida BOOLEAN DEFAULT FALSE,
+    prioridade VARCHAR(20) DEFAULT 'Baixa',
+    categoria VARCHAR(50),
+    usuario_id INT NOT NULL,
+    FOREIGN KEY (usuario_id) REFERENCES usuario(id) ON DELETE CASCADE
+);

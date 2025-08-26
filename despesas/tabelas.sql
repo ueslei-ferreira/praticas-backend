c7f563d449ad31a035028e8d704fc642eb3ae0dc
Ueslei GIT <ueslei392@gmail.com>
2025-08-26 11:14:01 -0300

    add: modelagem tabelas, configs JWT

--- despesas/tabelas.sql
+++ despesas/tabelas.sql
@@ -1,6 +1,13 @@
-CREATE TABLE tarefa(
+CREATE TABLE categoria_despesas(
     id SERIAL PRIMARY KEY,
-    categoria VARCHAR(100) NOT NULL,
+    tipo VARCHAR(50) UNIQUE NOT NULL
+);
+
+INSERT INTO categoria_despesas (tipo) VALUES ("mercado"),("lazer"),("eletrônicos"),("saúde"),("roupas"),("outros");
+
+CREATE TABLE despesa(
+    id SERIAL PRIMARY KEY,
     vencimento DATE,
-    montante DECIMAL(10,2)
+    montante DECIMAL(10,2),
+    tipo_despesas_id INT NOT NULL references categoria_despesas(id),
 );
